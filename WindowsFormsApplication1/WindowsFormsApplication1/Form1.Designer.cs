0bc2d14c34bebd3ea07ee25d41a0e329668a031c
M3azHb <167322790+M3azHb@users.noreply.github.com>
2024-05-13 14:56:27 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.player1 = new System.Windows.Forms.Label();
+            this.player2 = new System.Windows.Forms.Label();
+            this.O = new System.Windows.Forms.Label();
+            this.T = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.X = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.a1.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.a1.Location = new System.Drawing.Point(58, 42);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(150, 145);
+            this.a1.TabIndex = 0;
+            this.a1.TabStop = false;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.a3.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.a3.Location = new System.Drawing.Point(400, 43);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(150, 145);
+            this.a3.TabIndex = 1;
+            this.a3.TabStop = false;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.a2.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.a2.Location = new System.Drawing.Point(230, 42);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(150, 145);
+            this.a2.TabIndex = 2;
+            this.a2.TabStop = false;
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.b2.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b2.Location = new System.Drawing.Point(230, 208);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(150, 145);
+            this.b2.TabIndex = 5;
+            this.b2.TabStop = false;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.b3.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b3.Location = new System.Drawing.Point(400, 208);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(150, 145);
+            this.b3.TabIndex = 4;
+            this.b3.TabStop = false;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.b1.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b1.Location = new System.Drawing.Point(58, 208);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(150, 145);
+            this.b1.TabIndex = 3;
+            this.b1.TabStop = false;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.c2.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.c2.Location = new System.Drawing.Point(230, 375);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(150, 145);
+            this.c2.TabIndex = 8;
+            this.c2.TabStop = false;
+            this.c2.UseVisualStyleBackColor = false;
+            this.c2.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.c3.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.c3.Location = new System.Drawing.Point(400, 375);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(150, 145);
+            this.c3.TabIndex = 7;
+            this.c3.TabStop = false;
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.c1.Font = new System.Drawing.Font("Myanmar Text", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.c1.Location = new System.Drawing.Point(58, 375);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(150, 145);
+            this.c1.TabIndex = 6;
+            this.c1.TabStop = false;
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.bins_Click);
+            // 
+            // player1
+            // 
+            this.player1.AutoSize = true;
+            this.player1.BackColor = System.Drawing.Color.Black;
+            this.player1.Font = new System.Drawing.Font("News706 BT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1.ForeColor = System.Drawing.Color.SandyBrown;
+            this.player1.Location = new System.Drawing.Point(73, 564);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(164, 47);
+            this.player1.TabIndex = 9;
+            this.player1.Text = "player1";
+            // 
+            // player2
+            // 
+            this.player2.AutoSize = true;
+            this.player2.BackColor = System.Drawing.Color.Black;
+            this.player2.Font = new System.Drawing.Font("News706 BT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2.ForeColor = System.Drawing.Color.SandyBrown;
+            this.player2.Location = new System.Drawing.Point(412, 564);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(164, 47);
+            this.player2.TabIndex = 10;
+            this.player2.Text = "player2";
+            // 
+            // O
+            // 
+            this.O.AutoSize = true;
+            this.O.BackColor = System.Drawing.Color.Transparent;
+            this.O.ForeColor = System.Drawing.Color.Brown;
+            this.O.Location = new System.Drawing.Point(453, 637);
+            this.O.Name = "O";
+            this.O.Size = new System.Drawing.Size(52, 70);
+            this.O.TabIndex = 11;
+            this.O.Text = "0";
+            // 
+            // T
+            // 
+            this.T.AutoSize = true;
+            this.T.BackColor = System.Drawing.Color.Transparent;
+            this.T.ForeColor = System.Drawing.Color.Brown;
+            this.T.Location = new System.Drawing.Point(276, 637);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(52, 70);
+            this.T.TabIndex = 12;
+            this.T.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("News706 BT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label5.Location = new System.Drawing.Point(271, 564);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 47);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "eq";
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.BackColor = System.Drawing.Color.Transparent;
+            this.X.ForeColor = System.Drawing.Color.Brown;
+            this.X.Location = new System.Drawing.Point(106, 637);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(52, 70);
+            this.X.TabIndex = 14;
+            this.X.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Font = new System.Drawing.Font("News706 BT", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(609, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 75);
+            this.button1.TabIndex = 15;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(556, 111);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(150, 33);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "New Game";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 70F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(859, 818);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.T);
+            this.Controls.Add(this.O);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a1);
+            this.Font = new System.Drawing.Font("Myanmar Text", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(9, 13, 9, 13);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X&O";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Label player1;
+        private System.Windows.Forms.Label player2;
+        private System.Windows.Forms.Label O;
+        private System.Windows.Forms.Label T;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label X;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool m = true;
+        int m_count = 0;
+
+        private void bins_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (m)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            m = !m;
+            b.Enabled = false;
+            m_count++;
+            checkwinner();
+        }
+private void checkwinner()
+{
+    bool i = false;
+
+    if((a1.Text==a2.Text)&&(a2.Text==a3.Text)&&(!a1.Enabled))
+{
+    i = true;
+}
+    
+    else if((b1.Text==b2.Text)&&(b2.Text==b3.Text)&&(!b1.Enabled))
+{
+       i = true;
+}
+
+    else if((c1.Text==c2.Text)&&(c2.Text==c3.Text)&&(!c1.Enabled))
+{
+       i = true;
+}
+    else if ((a1.Text == b1.Text) && (b1.Text == c1.Text) && (!a1.Enabled))
+    {
+        i = true;
+    }
+    else if ((a2.Text == b2.Text) && (b2.Text == c2.Text) && (!a2.Enabled))
+    {
+        i = true;
+    }
+    else if ((a3.Text == b3.Text) && (b3.Text == c3.Text) && (!a3.Enabled))
+    {
+        i = true;
+    }
+    else if ((a1.Text == b2.Text) && (b2.Text == c3.Text) && (!a1.Enabled))
+    {
+        i = true;
+    }
+    else if ((a3.Text == b2.Text) && (b2.Text == c1.Text) && (!a3.Enabled))
+    {
+        i = true;
+    } if (i)
+    {
+        string s;
+        if (m)
+        {
+            int a = Convert.ToInt32(O.Text);
+            a++;
+            O.Text = a.ToString();
+            s = player2.Text;
+            enabledbuttons();
+        }
+        else
+        {
+            int a = Convert.ToInt32(X.Text);
+            a++;
+            X.Text = a.ToString();
+            s = player1.Text;
+            enabledbuttons();
+        }
+        MessageBox.Show(s + " win ");
+        m_count = 0;
+    }
+        if (m_count == 9) 
+          {
+              MessageBox.Show(" TA3ADOOOL ");
+              m_count = 0;
+              enabledbuttons();
+              int a = Convert.ToInt32(T.Text);
+              a++;
+              T.Text = a.ToString();
+          }
+
+} 
+        private void enabledbuttons()
+       {
+           foreach (Control c in Controls)
+           {
+               try
+               {
+                   Button b = (Button)c;
+                   b.Enabled = true;
+                   b.Text = "";
+
+               }
+               catch { }
+           } 
+       }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            enabledbuttons();
+            O.Text = "0";
+            X.Text = "0";
+            T.Text = "0";
+            m_count = 0;
+        }
+
+
+    }
+}
